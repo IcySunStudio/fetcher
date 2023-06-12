15f85b8cacb3f85b7469d921af939a76b8f70f0b
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-06-12 09:54:08 +0200

    Improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,12 +31,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-  Future<String> fetchTask() async {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _fetchController = ParameterizedFetchBuilderController<bool, String>();
+
+  bool withError = false;
+  bool dataClear = false;
+
+  Future<String> fetchTask(bool? withError) async {
     final response = await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/Paris'));
     await Future.delayed(const Duration(seconds: 2));
+    if (withError == true) throw Exception('Error !');
     return json.decode(response.body)['datetime'];
   }
 
@@ -46,26 +57,89 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Fetcher Example'),
       ),
-      body: FetchBuilder.basic<String>(
-        task: fetchTask,
-        builder: (context, data) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Data is fetched :',
-                style: Theme.of(context).textTheme.titleMedium,
-                textAlign: TextAlign.center,
-              ),
-              Text(
-                data,
-                textAlign: TextAlign.center,
-              ),
-            ],
-          );
-        },
+      body: Column(
+        children: [
+
+          // Header
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text('Fetcher with parameters'),
+          ),
+
+          // Settings
+          CheckboxListTile(
+            title: const Text('Clear data first'),
+            value: dataClear,
+            onChanged: (value) {
+              setState(() {
+                dataClear = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: const Text('With error'),
+            value: withError,
+            onChanged: (value) {
+              setState(() {
+                withError = value!;
+              });
+            },
+          ),
+
+          // Buttons
+          ElevatedButton(
+            onPressed: () => _fetchController.refresh(clearDataFirst: dataClear, param: withError),
+            child: const Text('Refresh'),
+          ),
+
+          // Fetcher
+          Expanded(
+            child: FetchBuilder<bool, String>.parameterized(
+              controller: _fetchController,
+              task: fetchTask,
+              builder: (context, data) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Data is fetched :',
+                      style: Theme.of(context).textTheme.titleMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      data,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          const Separator(),
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text('Dense Fetcher with Error'),
+          ),
+          FetchBuilder.basic<String>(
+            task: () async => throw Exception('Error !!'),
+            isDense: true,
+            builder: (context, data) => throw StateError('Should never reach this code'),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Separator extends StatelessWidget {
+  const Separator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      color: Colors.grey,
     );
   }
 }
