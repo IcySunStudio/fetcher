21dd3fb8953a1153b975186a3680823fd1beab2a
Nico04 <34476051+Nico04@users.noreply.github.com>
2026-06-10 16:49:49 +0200

    implement equality and hashCode for DataWrapper

--- lib/src/utils/data_wrapper.dart
+++ lib/src/utils/data_wrapper.dart
@@ -4,4 +4,14 @@ class DataWrapper<T> {
   const DataWrapper(this.data);
 
   final T data;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DataWrapper<T> &&
+          runtimeType == other.runtimeType &&
+          data == other.data;
+
+  @override
+  int get hashCode => data.hashCode;
 }
